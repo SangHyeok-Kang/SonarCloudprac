f74884489897bc90f9ca3143de88bd26695484a2
SangHyeok-Kang <tdkgur200002@naver.com>
2024-04-06 21:59:30 +0900

    project 생성

---
+++ src/main/java/com/example/sonarcloudprac/SonarcloudpracApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarcloudprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarcloudpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarcloudpracApplication.class, args);
+	}
+
+}
