cfbbe5b1019d7024784c41c8f0a9278cd5777dfa
SangHyeok-Kang <tdkgur200002@naver.com>
2024-04-06 22:39:46 +0900

    sonarcloud 동작 확인

---
+++ src/main/java/com/example/sonarcloudprac/practice.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.sonarcloudprac;
+
+/**
+ *
+ * @author Hello World
+ */
+public class practice {
+    public void practice(){
+        System.out.println("hello world");
+    }
+}
